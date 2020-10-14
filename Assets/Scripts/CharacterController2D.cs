0d2247974544e34651c0c30cfede75d365e889cc
arebena <rebena.arthur@gmail.com>
2020-10-14 15:03:56 +0200

    initial

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -113,13 +113,11 @@ public class CharacterController2D : MonoBehaviour
 			// If the input is moving the player right and the player is facing left...
 			if (move > 0 && !m_FacingRight)
 			{
-				// ... flip the player.
 				Flip();
 			}
 			// Otherwise if the input is moving the player left and the player is facing right...
 			else if (move < 0 && m_FacingRight)
 			{
-				// ... flip the player.
 				Flip();
 			}
 		}
